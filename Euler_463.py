bc7991e91ecdf01d3e13bed4420811224770fd0f
HoagyC <hoagycunningham@gmail.com>
2018-01-02 17:39:57 +0000

    463 stuck

--- Euler_463.py
+++ Euler_463.py
@@ -1,10 +1,10 @@
 # The function f is defined for all positive integers as follows:
 
-# f(1)=1f(1)=1
-# f(3)=3f(3)=3
-# f(2n)=f(n)f(2n)=f(n)
-# f(4n+1)=2f(2n+1)−f(n)f(4n+1)=2f(2n+1)−f(n)
-# f(4n+3)=3f(2n+1)−2f(n)f(4n+3)=3f(2n+1)−2f(n)
+# f(1)=1
+# f(3)=3
+# f(2n)f(n)
+# f(4n+1)=2f(2n+1)−f(n)
+# f(4n+3)=3f(2n+1)−2f(n)
 # The function S(n)S(n) is defined as ∑ni=1f(i)∑i=1nf(i).
 
 # S(8)=22S(8)=22 and S(100)=3604S(100)=3604.
@@ -14,7 +14,7 @@
 import math
 
 def oddtri(n):
-    tri = math.floor((n+1)//2)
+    tri = math.floor((n+1)/2)
     total = (tri*tri+1)-1
     print(total)
     return(total)
@@ -24,7 +24,7 @@ def run(m):
     count = 0
     for i in range(m.bit_length()):
         count += oddtri(r)
-        r = r//2
+        r = r/2
     return(count)
 
 if __name__ == "__main__":

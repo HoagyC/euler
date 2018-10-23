76f4ccee9b72051ef6756e850377bee258debba1
HoagyC <hoagycunningham@gmail.com>
2018-10-23 20:27:06 +0100

    Update Euler_639:2.py

--- Euler_639:2.py
+++ Euler_639:2.py
@@ -5,24 +5,33 @@ import time
 def run(n,p):
     t = time.clock()
     count=0
+    # only need primes up to sqrt(n)
     primes=getPrimes(math.floor(math.sqrt(n)))
     i = 2
     pl=len(primes)-1
+    # goes through from i = 0 to 1e12 (no for loop cause the range is too large)
     while i < n+1:
         k=i
         j=0
         factors=[]
         while True:
+            # goes through the list of primes for divisors
+            # when it finds one it appends it to factors
             if k%primes[j]==0:
                 factors.append(primes[j])
             while k%primes[j]==0:
+                # keeps dividing by the found factor as long as possible
                 k/=primes[j]
             if k < (primes[j]**2-1) or j==pl:
+                
                 break
             j+=1
         if k>1:
+            # primes can only have one prime factor above sqrt(n)
+            # after removing all lower prime factors, this is what remains
             factors.append(int(k))
         for m in range(2,p+1):
+            # add calculates the multiplicative function for power m, s.t. modulo
             add=1
             for j in range(len(factors)):
                 add*=((factors[j]**p)%1000000007)
@@ -33,6 +42,7 @@ def run(n,p):
     print((count+1)%1000000007)
     
 def getPrimes(n):
+    # prime sieve up to n
     numbers = list(range(2,n+1))
     for i in range(len(numbers)):
         if numbers[i] != 0:
